ff826dd0ef763cfd95dfaae36db1c5f837f771c0
Guyutongxue <guyutongxue@163.com>
2023-03-21 01:14:42 +0800

    add highlight

--- README.md
+++ README.md
@@ -11,3 +11,10 @@
 由于读取私有仓库使用了 Guyutongxue 的 PAT，而 PAT 只有一年有效期。所以每年三月都需要手动延长。如果 Guyutongxue 失踪了无法延长，那么请手动生成 PAT，把它设置到本仓库的 `DATA_REPO_PAT` 秘密中。
 
 如果 Guyutongxue 手欠修改了他的 OJ 密码，那么请当期助教把自己的用户名密码设置到本仓库的 `OPENJUDGE_USERNAME` 与 `OPENJUDGE_PASSWORD` 秘密中。
+
+## 本地开发
+
+- 新建 `.env` 文件写入 `OPENJUDGE_USERNAME` 与 `OPENJUDGE_PASSWORD`。
+- 新建 `data` 目录符号链接到 `pku-software/homework-answer-data`。
+- 运行 `pnpm build:data` 准备数据。
+- 运行 `pnpm dev` 热预览。
--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -28,6 +28,16 @@ function getSidebar(): SidebarConfigArray {
     return sidebar;
 }
 
+function codeToBlock(code: string, prefixNum: number, suffixNum: number) {
+    code = code.trim();
+    const lineNum = code.split("\n").length;
+    let focus = "";
+    if (prefixNum && suffixNum) {
+        focus = `{${1 + prefixNum}-${lineNum - suffixNum}}`;
+    }
+    return `\n\`\`\`cpp${focus}\n` + code + "\n```\n";
+}
+
 async function onInitialized(app: App) {
     const pages: Promise<Page>[] = [];
     for (const contest of contests) {
@@ -45,6 +55,8 @@ ${contest.problems.map((p) => `- [${p.title}](./t${p.id}.html)`).join("\n")}`,
         );
     }
     for (const [id, problem] of Object.entries(problems)) {
+        const prefixLineNum = problem.prefix.trim().split("\n").length;
+        const suffixLineNum = problem.suffix.trim().split("\n").length;
         pages.push(
             createPage(app, {
                 path: `/t${id}.html`,
@@ -58,24 +70,26 @@ ${problem.description}
 
 ### 关于输入
 
-${problem.input}
+${problem.input.replace(/\n/g, "\n\n")}
 
 ### 关于输出
 
-${problem.output}
+${problem.output.replace(/\n/g, "\n\n")}
 
 :::
 
-${problem.explanation}
+${problem.explanation.replace(/\n/g, "\n\n")}
 
 ## 参考答案
 
 ${
     typeof problem.codes === "string"
-        ? "```cpp\n" + problem.codes + "```"
+        ? codeToBlock(problem.codes, prefixLineNum, suffixLineNum)
         : "<CodeGroup>\n" +
           Object.entries(problem.codes).map(([title, code]) => {
-              return `<CodeGroupItem title="${title}">\n\n\`\`\`cpp\n${code}\n\`\`\`\n\n</CodeGroupItem>`;
+              return `<CodeGroupItem title="${title}">
+${codeToBlock(code, prefixLineNum, suffixLineNum)}       
+</CodeGroupItem>`;
           }) +
           "\n</CodeGroup>"
 }
